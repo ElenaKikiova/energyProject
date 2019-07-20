d8dc4b2a9b891631307c8aa1ba6642375b77ec07
elenakikiova <elenakikiova@mail.ru>
2019-07-20 17:35:53 +0300

    oh

---
+++ baseUrl.js
@@ -0,0 +1,12 @@
+// Url of the database
+
+// Depends on const online
+// const online = false;
+const online = true;
+
+var baseUrl = "mongodb://localhost:27017/energy";
+if(online == true){
+    baseUrl = "mongodb://Admin:energy2019@ds149744.mlab.com:49744/energy";
+}
+
+module.exports = baseUrl;
